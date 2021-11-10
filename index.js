b1a95cb74a5f2de187526f3704754d5d9e64ff7f
Nazmul Hasan Nahid <Nazmulhasan3394@gmail.com>
2021-11-11 00:17:13 +0600

    server connected

--- index.js
+++ index.js
@@ -11,6 +11,39 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 console.log(uri)
 
+
+async function run(){
+  try{
+     await client.connect();
+    // console.log('database successfully port run');
+
+    const database = client.db('product');
+    const productsCollection =database.collection('cycle');
+    
+    //GET API
+    app.get('/products', async (req ,res)=> {
+       const cursor = productsCollection.find({});
+       const  products = await cursor.toArray();
+       res.send(products);
+    })
+
+    //POST API
+    app.post('/products', async(req, res)=>{
+        const product = req.body;
+        const result = await productsCollection.insertOne(product);
+        res.json(result)
+    });
+  
+   
+
+  }
+  finally{
+      //await client.close();
+  }
+
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
   res.send('Hello Cycle mart')
 })
